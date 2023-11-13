e01c96ba967c89009c2c31014d9116e09ee86753
Francisco Javier Andreu García <javiandreugarcia@gmail.com>
2023-11-13 21:30:25 +0100

    update

--- jakarta/src/main/java/com/mycompany/jakarta/Jakarta.java
+++ jakarta/src/main/java/com/mycompany/jakarta/Jakarta.java
@@ -5,6 +5,10 @@
 
 package com.mycompany.jakarta;
 
+import jakarta.xml.bind.JAXBContext;
+import jakarta.xml.bind.JAXBException;
+import jakarta.xml.bind.Marshaller;
+import jakarta.xml.bind.PropertyException;
 import jakarta.xml.bind.annotation.XmlElement;
 /**
  *
@@ -13,7 +17,21 @@ import jakarta.xml.bind.annotation.XmlElement;
 public class Jakarta {
 
     public static void main(String[] args) {
-        
+        try {
+            Libro libro= new Libro("Odisea 2001",400);
+            JAXBContext contexto = JAXBContext.newInstance(
+                    libro.getClass() );
+                Marshaller marshaller = contexto.createMarshaller();
+                marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT,
+                    Boolean.TRUE);
+                marshaller.marshal(libro, System.out);
+        } catch (PropertyException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (JAXBException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
  
     }
        
