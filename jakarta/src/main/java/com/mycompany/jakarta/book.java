96f182c3709ad4e13f0302ea0d5032e740c49390
Francisco Javier Andreu García <javiandreugarcia@gmail.com>
2023-11-20 21:22:49 +0100

    .

---
+++ jakarta/src/main/java/com/mycompany/jakarta/book.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.jakarta;
+
+/**
+ *
+ * @author maget
+ */
+
+
+public class book {
+    private String title;
+    private int id;
+    private String author;
+    private String genre;
+    private double price;
+    private String publish_date;
+    private String description;
+}
